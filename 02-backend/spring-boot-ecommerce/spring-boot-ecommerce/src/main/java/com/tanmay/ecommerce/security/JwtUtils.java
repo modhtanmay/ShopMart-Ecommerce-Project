cfebef3b3fd25c206868b6c1dbefd15babbc142c
modhtanmay <modhtanmay@gmail.com>
2020-12-06 23:09:18 +0530

    initial commit added frontend and backend files

--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/tanmay/ecommerce/security/JwtUtils.java
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/tanmay/ecommerce/security/JwtUtils.java
@@ -13,10 +13,10 @@ import io.jsonwebtoken.*;
 public class JwtUtils {
 	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-	@Value("${bezkoder.app.jwtSecret}")
+	@Value("${tanmay.app.jwtSecret}")
 	private String jwtSecret;
 
-	@Value("${bezkoder.app.jwtExpirationMs}")
+	@Value("${tanmay.app.jwtExpirationMs}")
 	private int jwtExpirationMs;
 
 	public String generateJwtToken(Authentication authentication) {
